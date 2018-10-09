db2f9d8cc6a0321ccbe326542c2823a6c202a457
Son <>
2018-10-09 16:33:44 +0700

    no message

--- FoodTracker/FoodTracker/MealTableViewController.swift
+++ FoodTracker/FoodTracker/MealTableViewController.swift
@@ -10,12 +10,14 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
     var meals = [Meal]()
     var filterMeals = [Meal]()
     let searchController = UISearchController(searchResultsController: nil)
+    private var pendingRequestWorkItem: DispatchWorkItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //UI search bar
         // Use the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem
-
+        
         // Load any saved meals, otherwise load sample data.
         if let savedMeals = loadMeals() {
             meals += savedMeals
@@ -25,29 +27,30 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
             loadSampleMeals()
         }
         
-        filterMeals = meals
+        //filterMeals = meals
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
         searchController.hidesNavigationBarDuringPresentation = false
+        searchController.isActive = true
         definesPresentationContext = true
         navigationItem.searchController = searchController
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     //MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filterMeals.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -59,8 +62,8 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
         }
         
         // Fetches the appropriate meal for the data source layout.
-        let meal = filterMeals[indexPath.row]
         
+        let meal = filterMeals[indexPath.row]
         cell.nameLabel.text = meal.name
         cell.photoImageView.image = meal.photo
         cell.ratingControl.rating = meal.rating
@@ -68,7 +71,7 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
         return cell
     }
     
-
+    
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -76,7 +79,7 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
         return true
     }
     
-
+    
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
@@ -89,47 +92,44 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
             
             saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-
+    
     
     //MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare (for segue: UIStoryboardSegue, sender: Any?) {
         
         super.prepare(for: segue, sender: sender)
         
         switch(segue.identifier ?? "AddItem") {
-
+            
         case "AddItem":
             os_log("Adding a new meal.", log: OSLog.default, type: .debug)
-
+            
         case "ShowDetail":
             guard let mealDetailViewController = segue.destination as? MealViewController else {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
-
+            
             guard let selectedMealCell = sender as? MealTableViewCell else {
                 fatalError("Unexpected sender: \(sender)")
             }
-
+            
             guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
                 fatalError("The selected cell is not being displayed by the table")
             }
-
+            
             let selectedMeal = filterMeals[indexPath.row]
             mealDetailViewController.meal = selectedMeal
-
+            
         default:
             fatalError("Unexpected Segue Identifier; \(segue.identifier)")
         }
         saveMeals()
         
     }
-
     
     //MARK: Actions
     
@@ -138,30 +138,20 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
             
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 // Update an existing meal.
-//                filterMeals[selectedIndexPath.row] = meal
-//                meals[selectedIndexPath.row] = meal
-//                filterMeals = meals
                 if let index = meals.index(of: filterMeals[selectedIndexPath.row]){
                     meals[index] = meal
                     filterMeals[selectedIndexPath.row] = meal
+                    searchController.isActive = true
                 }
-                tableView.reloadRows(at: [selectedIndexPath], with: .none)
-//                if let index = meals.index(of: filterMeals[indexPath.row]){
-//                    meals.remove(at: index)
-//                    filterMeals.remove(at: indexPath.row)
-//                }
+                //tableView.reloadRows(at: [selectedIndexPath], with: .none)
             }
             else {
                 // Add a new meal.
-                
-                let newIndexPath = IndexPath(row: filterMeals.count, section: 0)
                 meals.append(meal)
                 filterMeals = meals
-                tableView.reloadRows(at: [newIndexPath], with: .automatic)
-//                meals.append(meal)
-//                filterMeals = meals
+                searchController.isActive = true
+                tableView.reloadData()
             }
-            
             // Save the meals.
             saveMeals()
         }
@@ -174,19 +164,19 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
         let photo1 = UIImage(named: "meal1")
         let photo2 = UIImage(named: "meal2")
         let photo3 = UIImage(named: "meal3")
-
+        
         guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
             fatalError("Unable to instantiate meal1")
         }
-
+        
         guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
             fatalError("Unable to instantiate meal2")
         }
-
+        
         guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
             fatalError("Unable to instantiate meal2")
         }
-
+        
         meals += [meal1, meal2, meal3]
     }
     
@@ -197,13 +187,18 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
         } else {
             os_log("Failed to save meals...", log: OSLog.default, type: .error)
         }
+//        let userDefault = UserDefaults.standard
+//        userDefault.set(meals, forKey: "meals")
     }
     
     private func loadMeals() -> [Meal]?  {
         return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+//        let userDefault = UserDefaults.standard
+//        return userDefault.object(forKey: "meals") as? [Meal]
     }
-    
+    // Search
     func updateSearchResults(for searchController: UISearchController) {
+        
         if let searchText = searchController.searchBar.text, !searchText.isEmpty {
             filterMeals = meals.filter{
                 item in
@@ -215,7 +210,7 @@ class MealTableViewController: UITableViewController, UISearchResultsUpdating {
         }
         tableView.reloadData()
     }
-
+    
 }
 
 
